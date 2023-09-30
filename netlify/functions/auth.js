05c9b73100ef599c22f46b687e91b23f6179330f
Kareem Younes <official.kyounes@gmail.com>
2023-09-30 23:14:28 +0300

    Fix image upload

---
+++ netlify/functions/auth.js
@@ -0,0 +1,25 @@
+const ImageKit = require('imagekit');
+
+export async function handler (event, context)
+{
+    console.log("Inside Auth")
+//   console.log("Event", event, "context", context)
+
+  const imagekit = new ImageKit({
+    urlEndpoint: 'https://ik.imagekit.io/lgd9ykfw6',
+    publicKey: 'public_jYTemusiZpt+yCs8inkps77IdKo=',
+    privateKey: 'private_H3sFP6kHVAlIkVKwf92WZZAQhDM='
+  });
+
+  const res = imagekit.getAuthenticationParameters()
+  console.log(res)
+  console.log(JSON.stringify(res))
+//   const data = await res.json()
+//   console.log("data", data)
+
+    return {
+      statusCode: 200,
+      ok: true,
+      body: JSON.stringify(res),
+    };
+}
